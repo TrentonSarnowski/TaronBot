669a305a3334eaca232a9e49f6ae0f333d0b1b02
deef-razer <deef-razer@LAPTOP-FFEV1GB5.nebula.msoe.edu>
2016-12-07 12:59:12 -0600

    inital Branch Commit

--- src/com/company/TaronBot/ServerCommunication.java
+++ src/com/company/TaronBot/ServerCommunication.java
@@ -3,7 +3,7 @@ package com.company.TaronBot;
 public class ServerCommunication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Create examples of how to communicate with the server
 
 	}
 
