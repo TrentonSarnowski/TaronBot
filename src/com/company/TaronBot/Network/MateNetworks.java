55d57e5eb6968ada94df301846f1d1b04e5d631d
deef-razer <deef-razer@LAPTOP-FFEV1GB5.nebula.msoe.edu>
2016-12-15 14:42:14 -0600

    updated networks and mate Networks to use a single arrayList and array
    This is in the middle of a refactor. Do not use. Will not compile.

--- src/com/company/TaronBot/Network/MateNetworks.java
+++ src/com/company/TaronBot/Network/MateNetworks.java
@@ -51,48 +51,34 @@ public class MateNetworks {
 				layers.add(new NetworkLayer(net0.getLayer(a).getPreviousLayerDimensions(), net0.getLayer(a).getOutputLayerDimensions(), net0.getLayer(a).getFunction()));
 			}
 			//for each output of the network layer
-			for(int q = 0; q < layers.get(0).getOutputLayerDimensions()[0]; q++){
-				for(int w = 0; w < layers.get(0).getOutputLayerDimensions()[1]; w++){
-					for(int e = 0; e < layers.get(0).getOutputLayerDimensions()[2]; e++){
-						for(int r = 0; r < layers.get(0).getOutputLayerDimensions()[3]; r++){
-							
-							//go through each input for that output
-							for(int i = 0; i < layers.get(0).getPreviousLayerDimensions()[0]; i++){
-								for(int j = 0; j < layers.get(0).getPreviousLayerDimensions()[1]; j++){
-									for(int k = 0; k < layers.get(0).getPreviousLayerDimensions()[2]; k++){
-										for(int l = 0; l < layers.get(0).getPreviousLayerDimensions()[3]; l++){
-											//a = layer in question
-											//qwer = output of layer (arrayList)
-											//ijkl = input from previous layer (mutator array)
-											workingArray.clear();
-											for(TakNetwork currentNet: networks){
-												//add the values in the other networks to the list of numbers in the array
-												//TODO FIGURE OUT WHY THE PROGRAM IS CRASHING					
-												//System.out.println(a+":"+q+":"+w+":"+e+":"+r+":"+i+":"+j+":"+k+":"+l);
-												workingArray.add(currentNet.getLayer(a).getMutatorArray(q, w, e, r)[i][j][k][l]);
-											}
-											
-											
-											statsArray = new double[workingArray.size()];
-											for(int n = 0; n < workingArray.size() ; n ++ ){
-												statsArray[n] = workingArray.get(n);
-											}
-											
-											random = new ISAACRandom();
-											random.setSeed(rand.nextInt());
-											NormalDistribution n = new NormalDistribution(random, mean.evaluate(statsArray), std.evaluate(statsArray));
-											
-											for(int layer = 0; layer < numNetworks; layer++){
-												layers.get(layer).getMutatorArray(q,w,e,r)[i][j][k][l] = n.sample();
-											}
-										}
-									}
-								}
-							}
-							
-							
-						}
+			for(int q = 0; q < layers.get(0).getOutputLayerSize(); q++){
+				for(int w = 0; w < layers.get(0).getInputLayerSize(); w++){
+
+					//a = layer in question
+					//qwer = output of layer (arrayList)
+					//ijkl = input from previous layer (mutator array)
+					workingArray.clear();
+					for(TakNetwork currentNet: networks){
+						//add the values in the other networks to the list of numbers in the array
+						//TODO FIGURE OUT WHY THE PROGRAM IS CRASHING					
+						//System.out.println(a+":"+q+":"+w+":"+e+":"+r+":"+i+":"+j+":"+k+":"+l);
+						workingArray.add(currentNet.getLayer(a).getMutatorArray(q)[w]);
 					}
+					
+					
+					statsArray = new double[workingArray.size()];
+					for(int n = 0; n < workingArray.size() ; n ++ ){
+						statsArray[n] = workingArray.get(n);
+					}
+					
+					random = new ISAACRandom();
+					random.setSeed(rand.nextInt());
+					NormalDistribution n = new NormalDistribution(random, mean.evaluate(statsArray), std.evaluate(statsArray));
+					
+					for(int layer = 0; layer < numNetworks; layer++){
+						layers.get(layer).getMutatorArray(q)[w] = n.sample();
+					}
+	
 				}
 			}
 			
