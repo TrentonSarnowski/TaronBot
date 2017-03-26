f27fa868cdbb7ff9f5dea14df8463495f60d9fce
deef <deef551@gmail.com>
2017-03-26 16:02:13 -0500

    fix colission issues and non compilation issues.

--- src/com/company/Testing/RNGTesting.java
+++ src/com/company/Testing/RNGTesting.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
-import com.company.TaronBot.NEAT.Network;
+import com.company.TaronBot.NEAT.Network.Network;
 
 import tech.deef.Tools.RNGTool;
 
