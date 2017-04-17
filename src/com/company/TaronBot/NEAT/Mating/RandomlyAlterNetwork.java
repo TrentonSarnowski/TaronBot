d0e8a01f4424f3d7cc0bdeaba4274044d902f1b0
Jeffrey Koehler <koehlerjj@msoe.edu>
2017-04-17 04:25:10 -0500

    finished the outlines for each of the parts.

--- src/com/company/TaronBot/NEAT/Mating/RandomlyAlterNetwork.java
+++ src/com/company/TaronBot/NEAT/Mating/RandomlyAlterNetwork.java
@@ -29,18 +29,66 @@ public class RandomlyAlterNetwork {
 			if(node.getClass()!=InputNode.class){
 				for(Node input: node.getInputNodes()){
 					if(helper.getChanceToChangeCurrentInputLinkage() > helper.getRandomGenerator().nextDouble()){
-						//change the linkage to another node
+						//change the linkage to another node 1 TODO
 					}
 					
 					if(helper.getChanceToRemoveLinkagePerNode() > helper.getRandomGenerator().nextDouble()){
-						//remove this linkage
+						//remove this linkage 2 TODO
+					}
+					
+					if(helper.getChanceToChangeMutatorValue() > helper.getRandomGenerator().nextDouble()){
+						//change mutator value 4 TODO
+						
+						if(helper.getChanceToReRandomizeMutatorValue() > helper.getRandomGenerator().nextDouble()){
+							//random change mutator value 5 TODO
+							
+						}else{
+							//change to mutator value increase or decrease. percentage??? TODO
+							
+						}
+					}
+					
+					if(helper.getChanceToReplaceLinkageWithNewNode()> helper.getRandomGenerator().nextDouble()){
+						//replace linkage with new node. 6 TODO
 					}
 				}
 				
 				if(helper.getChanceToAddLinkagePerNode() > helper.getRandomGenerator().nextDouble()){
-					//add a linkage
+					//add a linkage 3 TODO
 				}
 			}
+		}
+		
+		
+		int numNodesToAdd = 1;
+		int diff = helper.getMinimumNumberOfNewNodes() - helper.getMaximumNumberOfNewNodes();
+		numNodesToAdd += helper.getMinimumNumberOfNewNodes();
+		//add this number of nodes if the difference is more than 0;
+		if(diff != 0){
+			numNodesToAdd += helper.getRandomGenerator().nextInt(diff);
+		}
+		
+		for(int i = 0; i < numNodesToAdd; i++){
+			Node n1 = nodes.get(helper.getRandomGenerator().nextInt(nodes.size()));
+			Node n2 = nodes.get(helper.getRandomGenerator().nextInt(nodes.size()));
+			
+			//if same depth, break appart. 
+			if(n1.getNodeDepth() == n2.getNodeDepth()){
+				i--;
+				break;
+			}
+			//asumption,
+			//n1 is higher in the list, and therefore has a smaller number. 
+			//if it does not, switch them.
+			if(n1.getNodeDepth() > n2.getNodeDepth()){
+				Node temp = n1;
+				n2 = n1;
+				n2 = temp;
+			}
+			
+			//TODO create a new node here. 
+			
+			
 		}
 		
 		
@@ -52,21 +100,21 @@ public class RandomlyAlterNetwork {
 		
 		//todolist							^^^
 			//Lnkage modification			###
-				//change current linkage	###
-				//add linkage				###
-				//remove linkage			###
+				//change current linkage	###1
+				//add linkage				###2
+				//remove linkage			###3
 		
-			//mutator modification			@@@
-				//chance to change value	@@@
-					//maximum increase		@@@
-					//maximun decrease		@@@
-				//chance to randomize value	@@@
+			//mutator modification			###
+				//chance to change value	###4
+					//maximum increase		###
+					//maximun decrease		###
+					//randomize value		###
 	
-			//replace linkage with node		@@@
+			//replace linkage with node		###6
 		
-			//add new node to map			@@@
-				//maximum num of new nodes	@@@
-				//minimum num of new nodes	@@@
+			//add new node to map			###7
+				//maximum num of new nodes	###
+				//minimum num of new nodes	###
 		
 		return network;
 
